925c23eda38e7152f3c8e3e3a1d0e7d7d3158366
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-02 15:10:50 -0500

    removed comments

--- app/assets/javascripts/oven.js
+++ app/assets/javascripts/oven.js
@@ -7,23 +7,6 @@ var Oven = {
   bakeCookies: function() {
     for (var i = 0; i < this.cookies.length; i++) {
       this.cookies[i].bake();
-      // console.log(this.cookies[i].type + ": " + this.cookies[i].getState());
     }
   }
 };
-
-
-// pbCookie = new Cookie('peanut butter', 4);
-// oreoCookie = new Cookie('oreo', 6);
-
-// Oven.addCookie(pbCookie);
-// Oven.addCookie(oreoCookie);
-
-// Oven.bakeCookies();
-// Oven.bakeCookies();
-// Oven.bakeCookies();
-// Oven.bakeCookies();
-// Oven.bakeCookies();
-// Oven.bakeCookies();
-// Oven.bakeCookies();
-// Oven.bakeCookies();
