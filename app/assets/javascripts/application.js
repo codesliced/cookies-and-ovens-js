a9076ed0c09c92b3e0100b3e5c5f3ea6a2ecb695
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-02 11:19:43 -0500

    can move between prep table and oven

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,39 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+
+$(document).ready(function() {
+  var prepTable = new PrepTable();
+
+  $('#new_batch').submit(function(event) {
+    event.preventDefault();
+    var type = $('input[name=batch_type]').val(); 
+    var time = $('input[name=bake_time]').val(); 
+
+    $('input[name=batch_type]').val(""); 
+    $('input[name=bake_time]').val(""); 
+
+    var row = "<li><span class='cookie_type'>" + type + "</span><button class='add_to_oven'>Add to oven</button></li>";
+    $("#prep_batches").append(row);
+
+    var cookie = new Cookie(type, time);
+    prepTable.addCookie(cookie);
+  });
+
+  $('body').on('click', '.add_to_oven', function(response) {
+    var cookieType = $(this).closest('li').find('span').html();
+    if ($('#rack_0').html() === "[empty]") {
+      $('#rack_0').html(cookieType);
+      $(this).closest('li').remove();
+    } else if ($('#rack_1').html() === "[empty]") {
+      $('#rack_1').html(cookieType);
+      $(this).closest('li').remove();
+    } else if ($('#rack_2').html() === "[empty]") {
+      $('#rack_2').html(cookieType);
+      $(this).closest('li').remove();
+    } else {
+      alert("The oven is full, fool! Start baking already.")
+    }
+  });
+});
