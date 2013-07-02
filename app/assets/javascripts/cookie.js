6fae2a2045e0b5a58f80317d370ab1647709ec80
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-02 14:53:08 -0500

    fixed merge conflict

--- app/assets/javascripts/cookie.js
+++ app/assets/javascripts/cookie.js
@@ -11,34 +11,18 @@ Cookie.prototype = {
     this.getState();
   },
   getState: function() {
-<<<<<<< HEAD
-    if (this.timeInOven < this.bakeTime) {
-=======
     if (this.timeInOven < 1) {
       return "raw";
     } else if (this.timeInOven < this.bakeTime) {
->>>>>>> ac5e003c557a14fa5f3528a65c43aa2809ceca5e
       return "still gooey";
     } else if (this.timeInOven === this.bakeTime) {
       return "just right";
     } else if (this.timeInOven > this.bakeTime) {
       return "crispy";
     } else {
-<<<<<<< HEAD
-      return "raw";
-    }
-    // return this.state;
-=======
       return "something is wrong...";
     }
->>>>>>> ac5e003c557a14fa5f3528a65c43aa2809ceca5e
   }
 };
 
-// var cookie = new Cookie('chocolate chip', 5);
-// cookie.bake();
-// cookie.bake();
-// cookie.bake();
-// cookie.bake();
-// cookie.bake();
-// cookie.bake();
+
